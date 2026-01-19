80b73f64fa5d1cb6ec96784bfba8a4243880e2f9
nastianovikovai-sudo <nastia.novikova.i@gmail.com>
2026-01-19 19:20:56 +0300

    Update tbank.ts

--- src/lib/tbank.ts
+++ src/lib/tbank.ts
@@ -1,1 +1,7 @@
+import TbankPayments from "tbank-payments";
+
+export const tbank = new TbankPayments({
+  terminalKey: process.env.TBANK_TERMINAL_KEY!,
+  password: process.env.TBANK_PASSWORD!
+});
 
