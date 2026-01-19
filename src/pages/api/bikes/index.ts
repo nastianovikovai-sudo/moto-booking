190315ba8baa06e4c8db872fb8ee7d3aaf2952a4
nastianovikovai-sudo <nastia.novikova.i@gmail.com>
2026-01-19 19:23:57 +0300

    Update index.ts

--- src/pages/api/bikes/index.ts
+++ src/pages/api/bikes/index.ts
@@ -1,1 +1,8 @@
+import { db } from "@/lib/db";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(_: NextApiRequest, res: NextApiResponse) {
+  const bikes = await db.bike.findMany({ where: { isActive: true }, orderBy: { createdAt: "desc" } });
+  res.json(bikes);
+}
 
